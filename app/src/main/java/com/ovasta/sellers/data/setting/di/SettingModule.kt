e39ecdb5587fef40b340ca03325bed808201eba9
Farahat <m.farahat@maxab.io>
2026-05-25 16:56:12 +0300

    feat: update app DI to use shared DataStore factory

--- app/src/main/java/com/ovasta/sellers/data/setting/di/SettingModule.kt
+++ app/src/main/java/com/ovasta/sellers/data/setting/di/SettingModule.kt
@@ -1,10 +1,7 @@
 package com.ovasta.sellers.data.setting.di
 
-import android.content.Context
 import android.location.Geocoder
 import androidx.datastore.core.DataStore
-import androidx.datastore.core.DataStoreFactory
-import androidx.datastore.preferences.preferencesDataStoreFile
 import com.ovasta.sellers.data.setting.data.ISettingsLocalDataSource
 import com.ovasta.sellers.data.setting.data.ISettingsRemoteDataSource
 import com.ovasta.sellers.data.setting.data.ISettingsRepository
@@ -13,9 +10,10 @@ import com.ovasta.sellers.data.setting.data.SettingsRemoteDataSource
 import com.ovasta.sellers.data.setting.data.SettingsRepository
 import com.ovasta.sellers.base.constants.SharedPreferenceConstants
 import com.ovasta.sellers.data.setting.data.datastore.SessionPreferences
-import com.ovasta.sellers.data.setting.data.datastore.SessionPreferencesSerializer
+import com.ovasta.sellers.data.setting.data.datastore.createSessionDataStore
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
+import java.io.File
 import java.util.Locale
 
 val settingModule = module {
@@ -23,12 +21,9 @@ val settingModule = module {
     single<ISettingsRemoteDataSource> { SettingsRemoteDataSource(get()) }
     single<ISettingsLocalDataSource> { SettingsLocalDataSource(get<DataStore<SessionPreferences>>()) }
     single<DataStore<SessionPreferences>> {
-        DataStoreFactory.create(
-            serializer = SessionPreferencesSerializer,
-            produceFile = {
-                get<Context>().preferencesDataStoreFile(SharedPreferenceConstants.PREFERENCE_NAME)
-            }
-        )
+        createSessionDataStore {
+            File(androidContext().filesDir, "datastore/${SharedPreferenceConstants.PREFERENCE_NAME}").absolutePath
+        }
     }
     single<ISettingsRepository> { SettingsRepository(get(), get()) }
 }
