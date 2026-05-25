64f7f724dcdc35310070409859edb4ec2fbd29c6
Farahat <m.farahat@maxab.io>
2026-05-25 16:40:57 +0300

    feat: add shared LocalConstants

---
+++ shared/src/commonMain/kotlin/com/ovasta/sellers/base/constants/LocalConstants.kt
@@ -0,0 +1,10 @@
+package com.ovasta.sellers.base.constants
+
+object LocalConstants {
+    const val AppId = "com.ovasta.sellers"
+    const val KEY_APP_LANGUAGE = "KEY_APP_LANGUAGE"
+    const val COMM_PROTOCOL = "https://"
+    const val LANGUAGE_EN_ISO = "en"
+    const val LANGUAGE_AR_ISO = "ar"
+    const val DELAY_CLICK_ACTION = 500L
+}
