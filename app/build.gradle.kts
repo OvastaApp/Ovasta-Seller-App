c11925ab69a9dd5251fe989efea04b833bfcd7ba
Farahat <m.farahat@maxab.io>
2026-05-26 13:09:33 +0300

    feat: add device ID handling in settings and splash view model

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.ovasta.sellers"
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.0.6"
+        versionCode = 8
+        versionName = "1.0.8"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
     }
