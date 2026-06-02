9be4b65db67b0831a00bc5549a97b0ad55ddaf61
Farahat <m.farahat@maxab.io>
2026-06-02 21:14:32 +0300

    fix: set frameworks to static for iOS targets in build.gradle.kts

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -15,18 +15,21 @@ kotlin {
     iosX64 {
         binaries.framework {
             baseName = xcfName
+            isStatic = true
         }
     }
 
     iosArm64 {
         binaries.framework {
             baseName = xcfName
+            isStatic = true
         }
     }
 
     iosSimulatorArm64 {
         binaries.framework {
             baseName = xcfName
+            isStatic = true
         }
     }
 
