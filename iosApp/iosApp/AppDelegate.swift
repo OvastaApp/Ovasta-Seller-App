aaeae32c926db21f9d8204a099d4a328b9a20a58
Farahat <mohamed.ibrahem.farahat@gamil.com>
2026-06-17 14:49:59 +0300

    handle issues

--- iosApp/iosApp/AppDelegate.swift
+++ iosApp/iosApp/AppDelegate.swift
@@ -1,9 +1,10 @@
 import UIKit
 import Firebase
 import FirebaseMessaging
+import UserNotifications
 import sharedKit
 
-class AppDelegate: NSObject, UIApplicationDelegate, MessagingDelegate {
+class AppDelegate: NSObject, UIApplicationDelegate, MessagingDelegate, UNUserNotificationCenterDelegate {
 
     func application(
         _ application: UIApplication,
@@ -11,6 +12,19 @@ class AppDelegate: NSObject, UIApplicationDelegate, MessagingDelegate {
     ) -> Bool {
         FirebaseApp.configure()
         Messaging.messaging().delegate = self
+        
+        // Set up notification center delegate
+        UNUserNotificationCenter.current().delegate = self
+        
+        // Request notification permissions and register for remote notifications
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+            if granted {
+                DispatchQueue.main.async {
+                    application.registerForRemoteNotifications()
+                }
+            }
+        }
+        
         return true
     }
 
@@ -33,5 +47,14 @@ class AppDelegate: NSObject, UIApplicationDelegate, MessagingDelegate {
         print("Firebase FCM token: \(token)")
         UserDefaults.standard.set(token, forKey: "firebase_fcm_token")
         UserDefaults.standard.synchronize()
+    }
+    
+    // Handle foreground notifications
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+    ) {
+        completionHandler([.banner, .badge, .sound])
     }
 }
