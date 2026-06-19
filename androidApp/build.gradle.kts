60e452daadbcb42e4a366690c6e325ed69b9cd7a
Farahat <m.farahat@maxab.io>
2026-06-19 17:13:16 +0300

    chore: update version code and version name to 12 and 1.0.12

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.ovasta.sellers"
         minSdk = 24
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.0.11"
+        versionCode = 12
+        versionName = "1.0.12"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
     }
